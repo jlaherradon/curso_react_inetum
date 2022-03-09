fa69f2bd996006bb70ea5a754c73c958012b37e6
SECTORIALES\67259713 <juanluis.alvarez@inetum.world>
2022-03-09 12:39:11 +0100

    Práctica 5

--- miProyecto/index.js
+++ miProyecto/index.js
@@ -1,6 +1,6 @@
 import { registerRootComponent } from 'expo';
 
-import App from './todoApp/app/App';
+import App from './Listing-5.10-Flexwrap/App'
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
