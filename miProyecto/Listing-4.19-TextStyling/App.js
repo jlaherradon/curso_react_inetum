d72975fb5171c18d13a1c23eab10744177175257
SECTORIALES\67259713 <juanluis.alvarez@inetum.world>
2022-03-09 12:48:16 +0100

    Fix

--- miProyecto/Listing-4.19-TextStyling/App.js
+++ miProyecto/Listing-4.19-TextStyling/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View} from 'react-native';
 
-export default class App extends Component<{}> {
+export default class App extends Component {
     render() {
         return (
             <View style={styles.container}>
